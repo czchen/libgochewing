3bc0294b240d3318ee58657f466c905225e1e8bc
ChangZhuo Chen <czchen@gmail.com>
2013-05-31 23:57:36 +0800

    Implement NewPhrase

--- src/chewing/phrase.go
+++ src/chewing/phrase.go
@@ -1,7 +1,14 @@
 package chewing
 
+import (
+    "errors"
+    "fmt"
+    "strings"
+    "unicode/utf8"
+)
+
 type Word struct {
-    word string
+    word rune
     phone uint16
 }
 
@@ -19,6 +26,39 @@ type PhraseDictionary struct {
     root *PhraseTreeNode
 }
 
+func newPhrase(str string, bopomofo string, frequency uint32) (phrase *Phrase, err error) {
+    strLen := utf8.RuneCountInString(str)
+    bopomofoArray := strings.Split(bopomofo, " ")
+    bopomofoLen := len(bopomofoArray)
+
+    if strLen != bopomofoLen {
+        return nil, errors.New(fmt.Sprintf("len(%s) = %d, len(%s) = %d", str, strLen, bopomofo, bopomofoLen))
+    }
+
+    phrase = new(Phrase)
+    phrase.phrase = make([]Word, strLen)
+
+    for i := 0; i < strLen; i++ {
+        r, size := utf8.DecodeRuneInString(str)
+        if r == utf8.RuneError {
+            return nil, errors.New(fmt.Sprintf("`%s' contains invalid UTF8 character", str))
+        }
+
+        phone, err := convertBopomofoToPhone(bopomofoArray[i])
+        if err != nil {
+            return nil, err
+        }
+
+        phrase.phrase[i].word = r
+        phrase.phrase[i].phone = phone
+
+        str = str[size:]
+    }
+
+    phrase.frequency = frequency
+    return phrase, nil
+}
+
 func newPhraseTreeNode() (node *PhraseTreeNode) {
     node = new(PhraseTreeNode)
     node.children = make(map[uint16] *PhraseTreeNode)
@@ -31,16 +71,4 @@ func newPhraseDictionary() (dict *PhraseDictionary) {
     dict.root = newPhraseTreeNode()
 
     return dict
-}
-
-func (this *PhraseDictionary) insert(phrase string, phone []uint16, frequency uint32) {
-    currentNode := this.root
-    for _, item := range phone {
-        nextNode, ok := currentNode.children[item]
-        if !ok {
-            nextNode = newPhraseTreeNode()
-            currentNode.children[item] = nextNode
-        }
-        currentNode = nextNode
-    }
 }
--- src/chewing/phrase_test.go
+++ src/chewing/phrase_test.go
@@ -4,7 +4,27 @@ import (
     "testing"
 )
 
-func TestPhraseInsert(t *testing.T) {
-    dict := newPhraseDictionary()
-    dict.insert("測試", []uint16{ 10268, 8708 }, 0)
+func TestGetNewPhrase(t *testing.T) {
+    phrase, err := newPhrase("測試", "ㄘㄜˋ ㄕˋ", 10000)
+    if err != nil {
+        t.Errorf("newPhrase shall not fail. It fails with %s", err.Error())
+    }
+
+    if phrase.frequency != 10000 {
+        t.Errorf("frequency shall be %d, but it is %d", 10000, phrase.frequency)
+    }
+
+    var word Word
+
+    word.word = 0x6e2c
+    word.phone = 10268
+    if phrase.phrase[0] != word {
+        t.Errorf("word in Phrase is not expected value. %s", phrase.phrase[0])
+    }
+
+    word.word = 0x8a66
+    word.phone = 8708
+    if phrase.phrase[1] != word {
+        t.Errorf("word in Phrase is not expected value. %s", phrase.phrase[0])
+    }
 }
