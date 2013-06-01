11fc342148f569f80c9b7bf0ca84f4465092c083
ChangZhuo Chen <czchen@gmail.com>
2013-06-01 21:07:06 +0800

    Implement setupLogger and setupDictionary

--- src/chewing/chewing.go
+++ src/chewing/chewing.go
@@ -2,10 +2,14 @@ package chewing
 
 import (
     "bufio"
+    "errors"
+    "fmt"
     "os"
+    "strings"
 )
 
 type Chewing struct {
+    dict *PhraseDictionary
     logger ChewingLogger
 }
 
@@ -17,22 +21,73 @@ type ChewingParameters struct {
 
 func New(params *ChewingParameters) (chewing *Chewing, err error) {
     chewing = new(Chewing)
-    if params.logger == nil {
-        params.logger = new(ChewingDefaultLogger)
-    }
-    chewing.logger = params.logger
 
-    file, err := os.Open(params.phraseFile)
+    chewing.setupLogger(params)
+    err = chewing.setupDictionary(params)
     if err != nil {
-        chewing.logger.Printf("%s", err.Error())
         return nil, err
-    }
-    defer file.Close()
-
-    for scanner := bufio.NewScanner(file); scanner.Scan(); {
-        text := scanner.Text()
-        chewing.logger.Printf("%s", text)
     }
 
     return chewing, nil
+}
+
+func (this *Chewing) setupLogger(params *ChewingParameters) {
+    if params.logger == nil {
+        params.logger = new(ChewingDefaultLogger)
+    }
+    this.logger = params.logger
+}
+
+func (this *Chewing) setupDictionary(params *ChewingParameters) (err error) {
+    file, err := os.Open(params.phraseFile)
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+    this.dict = newPhraseDictionary()
+
+    for scanner := bufio.NewScanner(file); scanner.Scan(); {
+        text := scanner.Text()
+
+        comment := strings.Index(text, "#")
+        if comment != -1 {
+            text = text[:comment]
+        }
+
+        text = strings.TrimSpace(text)
+        if text == "" {
+            continue
+        }
+
+        token := strings.Split(text, " ")
+        if len(token) < 3 {
+            return errors.New(fmt.Sprintf("`%s' is invalid in phraseFile", text))
+        }
+
+        var frequency uint32
+        count, _:= fmt.Sscanf(token[1], "%d", &frequency)
+        if count != 1 {
+            return errors.New(fmt.Sprintf("`%s' is not a valid frequency", token[1]))
+        }
+
+        bopomofoSeq := token[2:]
+        phoneSeq := make([]uint16, len(bopomofoSeq))
+
+        for i := 0; i < len(bopomofoSeq); i++ {
+            phoneSeq[i], err = convertBopomofoToPhone(bopomofoSeq[i])
+            if err != nil {
+                return err
+            }
+        }
+
+        phrase, err := newPhrase(token[0], frequency)
+        if err != nil {
+            return err
+        }
+
+        this.dict.insertPhrase(phrase, phoneSeq)
+    }
+
+    return nil
 }
