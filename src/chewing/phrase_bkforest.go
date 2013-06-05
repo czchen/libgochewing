ed4b588521315e3ffc09d2febff3374f996051c9
ChangZhuo Chen <czchen@gmail.com>
2013-06-05 21:40:14 +0800

    Implement BK-forest query

--- src/chewing/phrase_bkforest.go
+++ src/chewing/phrase_bkforest.go
@@ -52,3 +52,53 @@ func (this *PhraseBKTreeNode) insert(phraseArrayItem *PhraseArrayItem) (err erro
         return this.children[distance].insert(phraseArrayItem)
     }
 }
+
+func (this *PhraseBKForest) query(phoneSeq []uint16, threshold uint8) (phraseArrayItem []*PhraseArrayItem) {
+    length := len(phoneSeq)
+    if this.tree[length] == nil {
+        return make([]*PhraseArrayItem, 0)
+    }
+
+    phraseArrayItem = make([]*PhraseArrayItem, 0, 2)
+    result := make(chan *PhraseArrayItem)
+    count := make(chan int)
+
+    counter := 1
+    go this.tree[length].query(phoneSeq, threshold, count, result)
+
+    for counter > 0 {
+        select {
+        case res := <- result:
+            if len(phraseArrayItem) == cap(phraseArrayItem) {
+                origin := phraseArrayItem
+                phraseArrayItem = make([]*PhraseArrayItem, 0, len(origin))
+                copy(phraseArrayItem, origin)
+            }
+            phraseArrayItem = phraseArrayItem[:len(phraseArrayItem) + 1]
+            phraseArrayItem[len(phraseArrayItem) - 1] = res
+        case c := <- count:
+            counter += c
+        }
+    }
+
+    return phraseArrayItem
+}
+
+func (this *PhraseBKTreeNode) query(phoneSeq []uint16, threshold uint8, count chan<- int, result chan<- *PhraseArrayItem) {
+    diff, err := calculateHammingDistance(phoneSeq, this.phraseArrayItem.phoneSeq)
+    if err != nil {
+        panic("calculateHammingDistance fails in PhraseBKTreeNode.query")
+    }
+    if diff <= threshold {
+        result <- this.phraseArrayItem
+    }
+
+    for i := diff - threshold; i <= diff + threshold; i++ {
+        if this.children[i] != nil {
+            count <- 1
+            go this.children[i].query(phoneSeq, threshold, count, result)
+        }
+    }
+
+    count <- -1
+}
