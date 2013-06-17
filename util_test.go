6517c861ce25b8d4d828b5a519c37bb94f62bb7a
ChangZhuo Chen <czchen@gmail.com>
2013-06-17 23:27:38 +0800

    Rewrite util_test.go with gocheck

--- util_test.go
+++ util_test.go
@@ -2,7 +2,6 @@ package libgochewing
 
 import (
 	"launchpad.net/gocheck"
-	"testing"
 )
 
 type TestData struct {
@@ -21,29 +20,15 @@ var DATA = []TestData{
 	},
 }
 
-func TestConvertBopomofoToPhone(t *testing.T) {
+func (this *DefaultSuite) TestConvertBopomofoAndPhone(c *gocheck.C) {
 	for _, data := range DATA {
 		phone, err := convertBopomofoToPhone(data.bopomofo)
-		if err != nil {
-			t.Errorf("convertBopomofoToPhone shall not return error %s", err.Error())
-		}
+		c.Check(phone, gocheck.Equals, data.phone)
+		c.Check(err, gocheck.IsNil)
 
-		if phone != data.phone {
-			t.Errorf("%s -> %d (shall be %d)", data.bopomofo, phone, data.phone)
-		}
-	}
-}
-
-func TestConvertPhoneToBopomofo(t *testing.T) {
-	for _, data := range DATA {
 		bopomofo, err := convertPhoneToBopomofo(data.phone)
-		if err != nil {
-			t.Errorf("convertPhoneToBopomofo shall not return error %s", err.Error())
-		}
-
-		if bopomofo != data.bopomofo {
-			t.Errorf("%d -> %s (shall be %s)", data.phone, bopomofo, data.bopomofo)
-		}
+		c.Check(bopomofo, gocheck.Equals, data.bopomofo)
+		c.Check(err, gocheck.IsNil)
 	}
 }
 
@@ -59,36 +44,8 @@ func (this *DefaultSuite) TestCalculateHammingDistance(c *gocheck.C) {
 	c.Check(func() { calculateHammingDistance(base, distPanic) }, gocheck.PanicMatches, `.*`)
 }
 
-func TestComparePhone(t *testing.T) {
-	var ret int
-	var x []uint16
-	var y []uint16
-
-	x = []uint16{8708}
-	y = []uint16{10268}
-	ret = comparePhoneSeq(x, y, 0)
-	if ret >= 0 {
-		t.Errorf("comparePhoneSeq(%s, %s, 0) shall < 0, but got %d", x, y, ret)
-	}
-
-	x = []uint16{10268}
-	y = []uint16{8708, 8708}
-	ret = comparePhoneSeq(x, y, 0)
-	if ret <= 0 {
-		t.Errorf("comparePhoneSeq(%s, %s, 0) shall >= 0, but got %d", x, y, ret)
-	}
-
-	x = []uint16{10262, 10262}
-	y = []uint16{10262, 10262}
-	ret = comparePhoneSeq(x, y, 0)
-	if ret != 0 {
-		t.Errorf("comparePhoneSeq(%s, %s, 0) shall = 0, but got %d", x, y, ret)
-	}
-
-	x = []uint16{10264}
-	y = []uint16{10268}
-	ret = comparePhoneSeq(x, y, PHONE_FUZZY_TONELESS)
-	if ret != 0 {
-		t.Errorf("comparePhoneSeq(%s, %s, PHONE_FUZZY_TONELESS) shall = 0, but got %d", x, y, ret)
-	}
+func (this *DefaultSuite) TestComparePhone(c *gocheck.C) {
+	c.Check(comparePhoneSeq([]uint16{8708}, []uint16{10268}, 0) < 0, gocheck.Equals, true)
+	c.Check(comparePhoneSeq([]uint16{10262, 10262}, []uint16{10262, 10262}, 0) == 0, gocheck.Equals, true)
+	c.Check(comparePhoneSeq([]uint16{10264}, []uint16{10268}, PHONE_FUZZY_TONELESS) == 0, gocheck.Equals, true)
 }
